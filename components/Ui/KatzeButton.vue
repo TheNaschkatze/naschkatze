eb82b7cb1f9d5581220e6b29226ffdadc1da1b97
theNaschkatze <jalo1412@gmail.com>
2023-03-21 21:04:45 +0100

    refactor: refactor button to use variants and not type

--- components/Ui/KatzeButton.vue
+++ components/Ui/KatzeButton.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
 const emit = defineEmits(['click'])
 withDefaults(defineProps<{
-  type?: "primary" | "secondary"
+  type?: "button" | "submit" | "reset"
+  variant?: "solid" | "ghost"
 }>(), {
-  type: "primary",
+  type: "button",
+  variant: "solid",
 })
 
 </script>
 
 <template>
   <button
-    tabindex="-1"
-    type="button"
+    :type="type"
     :class="[
         'inline-flex items-center rounded-md border justify-center border-transparent px-4 py-2 text-base font-medium',
         {
-          primary: 'bg-primary text-white shadow-sm hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2',
-          secondary: 'rounded-md bg-secondary py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-700 hover:bg-secondary-hover'
-        }[type]
+          solid: 'bg-primary text-white shadow-sm hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2',
+          ghost: 'rounded-md bg-secondary py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-700 hover:bg-secondary-hover'
+        }[variant]
     ]"
     @click="emit('click')"
   >
