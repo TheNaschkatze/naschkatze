f7ca9895e06e2a2ba7afd290810e6af239e9e615
theNaschkatze <jalo1412@gmail.com>
2023-04-03 22:45:58 +0200

    fix: margin

--- storyblok/BasePage.vue
+++ storyblok/BasePage.vue
@@ -5,7 +5,7 @@ defineProps<{blok: BasePage}>()
 </script>
 
 <template>
-  <div v-editable="blok" class="px-4">
+  <div v-editable="blok" class="lg:px-4">
     <StoryblokComponent v-for="nestedBlok in blok.body" :key="nestedBlok._uid" :blok="nestedBlok" />
   </div>
 </template>
